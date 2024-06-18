72e010c86c1d64106eb16d7953581627e1ae06e8
Baek Seung Yeop <tmdduq1999@naver.com>
2024-06-18 09:55:00 +0900

    setting

---
+++ src/test/java/hello/spharos/SpharosApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spharos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpharosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
