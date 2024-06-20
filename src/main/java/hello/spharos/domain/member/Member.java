777c4fabe866442773a6a15ad4c926a94985abb1
Baek Seung Yeop <tmdduq1999@naver.com>
2024-06-20 10:00:43 +0900

    member

--- src/main/java/hello/spharos/domain/member/Member.java
+++ src/main/java/hello/spharos/domain/member/Member.java
@@ -1,2 +1,16 @@
-package hello.spharos.domain.member;public class Member {
+package hello.spharos.domain.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+
 }
